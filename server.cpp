7de5518d6500c175c09304bac7731b2516ef9560
Emma Farin <efarin@z3r7p5.42lyon.fr>
2022-03-08 20:39:23 +0100

    poll stuff

--- server.cpp
+++ server.cpp
@@ -5,10 +5,17 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <string.h>
 
 int	main(){
-	int			sockfd, new_socket;
-	sockaddr_in	address;
+	int				sockfd, new_socket;
+	sockaddr_in		address;
+	int				ns;
+	struct pollfd	fds[200];
+	int				nfds = 1, current_size = 0;
+	bool			end_server = false;
 
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
 		std::cout << "Error\n";
@@ -21,22 +28,57 @@ int	main(){
 		std::cout << "Error, failed to bind\n";
 		exit(EXIT_FAILURE);
 	}
-	if (listen(sockfd, 3) < 0){
+	if (listen(sockfd, 1) < 0){
 		std::cout << "Error, failed to listen on socket\n";
 		exit(EXIT_FAILURE);
 	}
 	int addrlen = sizeof(address);
-	if ((new_socket = accept(sockfd, (struct sockaddr *)&address, 
-		(socklen_t*)&addrlen)) < 0){
-		std::cout << "Error, failed to grab connection\n";
-		exit(EXIT_FAILURE);
-	}
-	char		buffer[1024];
-	int			val_read;
-	
-	val_read = read(new_socket, buffer, 1024);
-	std::cout << "THE MESSAGE : " << buffer << std::endl;
-	std::string str = "thank's for the talk\n";
-	send(new_socket, str.data(), str.size(), 0);
+	memset(fds, 0 , sizeof(fds));
+	fds[0].fd = sockfd;
+	fds[0].events = POLLIN;
+	while (!end_server){
+		if ((new_socket = accept(sockfd, (struct sockaddr *)&address, 
+			(socklen_t*)&addrlen)) < 0){
+			std::cout << "Error, failed to grab connection\n";
+			exit(EXIT_FAILURE);
+		}
+		ns = poll(fds, nfds, 3 * 60 * 1000);
+		if (ns < 0){
+			std::cout << "Poll failed\n";
+			break;
+		}
+		else if (ns == 0){
+			std::cout << "Poll timed out\n";
+			break;
+		}
+		current_size = nfds;
+		for (int i = 0 ; i < current_size ; i++){
+			if(fds[i].revents != POLLIN)
+			{
+				printf("  Error! revents = %d\n", fds[i].revents);
+				end_server = true;
+				break;
+			}
+			if(fds[i].revents != POLLIN)
+			{
+				printf("  Error! revents = %d\n", fds[i].revents);
+				end_server = true;
+				break;
+			}
 
+		}
+		char		buffer[1024];
+		int			val_read;
+
+		fcntl(new_socket, F_SETFL, O_NONBLOCK);
+		val_read = read(new_socket, buffer, 1024);
+		std::cout << "THE MESSAGE : " << buffer << std::endl;
+		std::string str = "thank's for the talk\n";
+		send(new_socket, str.data(), str.size(), 0);
+	}
+
+	close(new_socket);
+	close(sockfd);
+
+	return 0;
 }
